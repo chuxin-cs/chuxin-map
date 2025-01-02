6a314d603d4798900bb700322451330e81f363ab
chuxin <15115359055@139.com>
2025-01-02 11:18:27 +0800

    feat: 学习useState

--- packages/react-api/src/App.tsx
+++ packages/react-api/src/App.tsx
@@ -1,13 +1,19 @@
-import { useState } from 'react'
+import {useState} from 'react'
+
+import {UseStatePage} from "./hook-api/useState";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <h1>Vite + React</h1>
-    </>
-  )
+    return (
+        <>
+            <h1>Vite + React {count}</h1>
+            <button onClick={() => setCount(count + 1)}>点击+1</button>
+
+            {/*  hooks api  */}
+            <UseStatePage/>
+        </>
+    )
 }
 
 export default App
---
+++ packages/react-api/src/hook-api/useState/index.tsx
@@ -0,0 +1,14 @@
+/**
+ * 由于是学习react阶段 useState api 代码都在当前文件中，不做文件拆分，影响
+ * */
+export function UseStatePage() {
+    return (
+        <div>
+            <h1>1、useState</h1>
+        </div>
+    )
+}
+
+
+
+
